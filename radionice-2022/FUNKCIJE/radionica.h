7fcc2a01ae0d2aa59af0100dc5523af294ecb45c
Timon-Robotika <116429264+Timon-Robotika@users.noreply.github.com>
2023-03-11 12:12:02 +0100

    Update radionica.h

--- radionice-2022/FUNKCIJE/radionica.h
+++ radionice-2022/FUNKCIJE/radionica.h
@@ -4,36 +4,45 @@
 
 void RobotLine::radionica() {
   static int broj = 0;
+  static int a = 4;
   if (lineAny()) {
 
 
-  if (broj < 2) {
-    if (line(1) and line(7)) {
-      go(-50, 50);
-      delayMs(500);
-      broj++;
+    if (broj < 2) {
+      if (line(1) and line(7)) {
+        go(-50, 50);
+        delayMs(500);
+        broj++;
+      }
     }
-    if (broj > 1) {
+    if (broj == 2) {
+      if (line(7) and line(8)) {
+        stop();
+        delayMs(500);
+        go(60, 60);
+        delayMs(300);
+        broj++;
+      }
+    }
+    if (broj == 3) {
       if (line(8) and line(0)) {
         go(-90, 90);
         delayMs(1000);
+        broj++;
       }
-      else if (line(7) or line(8)) {
-        go(60, 60);
-        delayMs(200);
-      }
-      else if (line(0) or line(1)) {
+    }
+    if (broj == 4) {
+      if (line(0) or line(1)) {
         go(90, -90);
         delayMs(500);
+        broj++;
 
       }
     }
 
-  }
-objectOnLine();
 
-}
-else{
-  go(60, 60);
-}
+    objectOnLine();
+
+  }
+
 }
