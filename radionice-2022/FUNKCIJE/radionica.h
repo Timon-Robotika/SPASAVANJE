7d23061a348f815b978c82b372c113cb769d5208
Timon-Robotika <116429264+Timon-Robotika@users.noreply.github.com>
2023-04-22 12:11:16 +0200

    Update radionica.h

--- radionice-2022/FUNKCIJE/radionica.h
+++ radionice-2022/FUNKCIJE/radionica.h
@@ -3,41 +3,18 @@
 
 
 void RobotLine::radionica() {
+  /*
+    static int broj = 0;
+    static int brzina = 0;
+    static int a = 0;
+    char simbol[10 + sizeof(char)];
 
-  static int broj = 0;
-  static int brzina = 0;
-  static int a = 0;
-  char simbol[10 + sizeof(char)];
-  if (broj == 1) {
-    while (!lineAny()) {
-      go(10, 55);
-      delayMs(700);
-      if (lineAny()) {
-        break;
-        broj++;
-      }
-      go(65, 10);
-      delayMs(750);
-      if (lineAny()) {
-        break;
-        broj++;
-      }
-      noLoopWithoutThis();
-      a++;
-      if (a == 1) {
-        broj++;
-      }
-    }
-  }
-
-
-  if (lineAny()) {
 
     if (broj == 0) {
       brzina = 0;
       if (line(0) and line(8) or line(1) and line(7)) {
-        go(-90, 90);
-        delayMs(500);
+        go(-40, 80);
+        delayMs(750);
         broj++;
       }
     }
@@ -45,20 +22,57 @@ void RobotLine::radionica() {
     if (broj == 1) {
       brzina = 0;
       if (line(0)) {
-        go(90, -90);
-        delayMs(500);
+        go(80, -40);
+        delayMs(750);
+        broj++;
       }
     }
     if (broj == 2) {
+      while (!lineAny()) {
+        go(10, 60);
+        delayMs(700);
+        if (lineAny()) {
+          break;
+          broj++;
+        }
+
+
+        go(60, 10);
+        delayMs(750);
+        if (lineAny()) {
+          break;
+          broj++;
+        }
+
+
+        go(60, 10);
+        delayMs(750);
+        if (lineAny()) {
+          break;
+          broj++;
+        }
+
+
+        go(10, 60);
+        delayMs(700);
+        if (lineAny()) {
+          break;
+          broj++;
+        }
+        noLoopWithoutThis();
+
+      }
+    }
+    if (broj == 3) {
       brzina = 1;
       if (line(7) and line(8)) {
-        go(100, 100);
+        go(90, 90);
         delayMs(150);
         broj++;
       }
     }
 
-    if (broj == 3) {
+    if (broj == 4) {
       brzina = 0;
       if (line(8) and line(0) or line(7) and line(1)) {
         go(-90, 90);
@@ -66,7 +80,7 @@ void RobotLine::radionica() {
         broj++;
       }
     }
-    if (broj == 4) {
+    if (broj == 5) {
       brzina = 0;
       if (line(0) or line(1)) {
         go(80, -40);
@@ -76,34 +90,63 @@ void RobotLine::radionica() {
       }
     }
 
-    if (broj == 5) {
+    if (broj == 6) {
       brzina = 1;
-      if (pitch() < -10){
+      if (pitch() < -10) {
         broj++;
       }
     }
-    if (broj == 6){
-      stop();
-      delayMs(1000000);
+    if (broj == 7) {
+      brzina = 1;
+      if (line(0) and line(8) or line(1) and line(7)) {
+        go(100, 80);
+        delayMs(500);
+        broj++;
+      }
+    }
+    if (broj == 8) {
+      brzina = 0;
+      if (pitch() < 10) {
+        broj++;
+      }
+    }
+
+    if (broj == 8) {
+      brzina = 0;
+    }
+
+
+
+
+    // kontrola brzine:
+    if (brzina == 0) {
+      if (broj == 3)
+        objectOnLine(0);
+      else
+        lineSlow();
+    }
+    else if (brzina == 1) {
+      if (broj == 2)
+        objectOnLine(3);
+      else
+        lineFast();
+    }
+
+
+
+
+    // Ne mjenjati, služi za pretvorbu vrste varijable i ispis na 8x8 led
+    sprintf(simbol, "%d", broj);
+    display(simbol);
+  */
+
+  int ms = millis();
+
+  while (millis() - ms > 2000) {
+    go(60, 60);
+    if (line(8) and line(0)) {
+      break;
     }
 
   }
-
-  // kontrola brzine:
-  if (brzina == 0) {
-    if (broj == 2)
-      objectOnLine(0);
-    else
-      lineSlow();
-  }
-  else if (brzina == 1) {
-    if (broj == 2)
-      objectOnLine(1);
-    else
-      lineFast();
-  }
-
-  // Ne mjenjati, služi za pretvorbu vrste varijable i ispis na 8x8 led
-  sprintf(simbol, "%d", broj);
-  display(simbol);
 }
