9827211530821b5c82651e7e94f92219e955de50
Timon-Robotika <116429264+Timon-Robotika@users.noreply.github.com>
2023-06-03 12:15:19 +0200

    Update radionica.h

--- radionice-2022/FUNKCIJE/radionica.h
+++ radionice-2022/FUNKCIJE/radionica.h
@@ -7,75 +7,74 @@ void RobotLine::radionica() {
   //  definiraj varijable
   char simbol[10 + sizeof(char)];
   static uint32_t ms = 0;
-  static int lijevo = 2;
-  static int naprijed = 2;
-  static int desno = 2;
-  static int a = 1;
+  // [lijevo, srednje, desno]
+  static int varijabla[] = {2, 2, 2};
+  static int a = 0;
+  int broj = 0;
 
-  // idi naprijed dok nisi na raskrzju
-  lineSlow();
 
   // ako vidi raskrzje
-  if (line(8) and line(0) or line(1) and line(7) and a > 4) {
-    //skreni 90 stupnjeva
-    go(-90, 90);
-    delayMs(500);
+  if ((line(8) and line(0) and (a<3)) or (line(1) and line(7)) and (a<3)) {
+    //skreni 90 stupnjeva i idi naprijed
+    go(-50, 100);
+    delayMs(650);
+    go(80, 80);
+    delayMs(400);
+
     // nakon 2 sek provjeri jeli ima crte
+
     ms = millis();
-    while (millis() - ms < 1800) {
+    while (millis() - ms < 1000) {
       go(40, 40);
-      if (!lineAny()) {
-        if (a == 1)
-          lijevo = 0;
-        if (a == 2)
-          naprijed = 0;
-        if (a == 3)
-          desno = 0;
-      }
-      else if (lineAny()) {
-        if (a == 1)
-          lijevo = 1;
-        if (a == 2)
-          naprijed = 1;
-        if (a == 3)
-          desno = 1;
-      }
+      if (lineAny())
+        broj++;
+      if (!lineAny())
+        broj--;
       noLoopWithoutThis();
     }
+    go(0, 0);
 
-    // 1 sek displayaj varijablu (1 ili 0)
-    stop();
-    if (a == 4){
-      lineSlow();
-      if (line(8) and line(0) or line(7) and line(1)){
-        go(60, 60);
-        delayMs(1500);
-        stop();
-        delayMs(100000);
-      }
+    // displayaj True ili False i stavi u listu T ili F
+    if (broj > 0) {
+      display("T");
+      if (a == 0)
+        varijabla[0] = 1;
+      if (a == 1)
+        varijabla[1] = 1;
+      if (a == 2)
+        varijabla[2] = 1;
     }
-    if (a == 3) {
-      sprintf(simbol, "%d", desno);
-      display(simbol);
-      delayMs(1000);
+    else if (broj < 0) {
+      display("F");
+      if (a == 0)
+        varijabla[0] = 0;
+      if (a == 1)
+        varijabla[1] = 0;
+      if (a == 2)
+        varijabla[2] = 0;
     }
-    else if (a == 2) {
-      sprintf(simbol, "%d", naprijed);
-      display(simbol);
-      delayMs(1000);
+    else {
+      display("0");
+
     }
-    else if (a == 1) {
-      sprintf(simbol, "%d", lijevo);
-      display(simbol);
-      delayMs(1000);
-    }
+
     // vrati se nazad
     go(90, -90);
     delayMs(1000);
     go(40, 40);
     delayMs(600);
+
     a++;
-//------------------------------- kad je a 4 on nece primjetit jer  je to sve u glavnom if statementu
+    sprintf(simbol, "%d", a);
+    display(simbol);
   }
+
+else{
+   // idi naprijed dok nisi na raskrzju
+  lineSlow();
+}
+
+
+
 
 }
